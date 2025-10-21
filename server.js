f766e197fa0e3b4de4ac1bcc90b625a38c73dbbf
Cody.McFarland <codymcfarland@protonmail.com>
2025-10-21 09:10:57 -0400

    save point

--- server.js
+++ server.js
@@ -51,13 +51,11 @@ const RESTAURANT = {
 }
 
 app.get('/', (req, res) => {
-  res.render('home.ejs'); {
-    
-  }
+  res.render('home.ejs', {restaurant: RESTAURANT});
 });
 
 app.get('/menu', (req, res) => {
-  res.render('menu.ejs');
+  res.render('menu.ejs', {menu: RESTAURANT.menu});
 });
 
 app.listen(3000);
